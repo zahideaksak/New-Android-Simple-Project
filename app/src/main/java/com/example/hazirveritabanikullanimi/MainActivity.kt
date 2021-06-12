d059ad0f1da7dcdb562f80d5e42530564ef73f23
zahideaksak <zahide.aksaak@gmail.com>
2021-06-12 20:05:30 +0300

    Button addded and toast message showed

--- app/src/main/java/com/example/hazirveritabanikullanimi/MainActivity.kt
+++ app/src/main/java/com/example/hazirveritabanikullanimi/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.hazirveritabanikullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.simpleButton)
+
+        button.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Butona Basıldı", Toast.LENGTH_SHORT).show()
+        })
     }
 }
